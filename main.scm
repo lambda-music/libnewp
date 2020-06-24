81ffff5b35a9ee296d5b501019ac6e3c032a1e99
Ats <oka.ats@gmail.com>
2020-06-24 13:34:04 +0900

    modified the main file

--- main.scm
+++ main.scm
@@ -1,6 +1,9 @@
 
-(define-if-not-defined newp-all '() )
+(if (defined? -DEFINED-LIBNEWP )
+    (abort '(aborted already-defined)))
 
+(define -DEFINED-LIBNEWP #t)
+(define newp-all '())
 (define (newp-clear)
   (for-each (lambda (x) (kilp x) ) newp-all )
   (set! newp-all '() )
@@ -9,8 +12,15 @@
 (define (newp-add v)
   (set! newp-all (xcons newp-all v)))
 
-; These identifies are obsolete; they are defined for the backward compatibility.
+; These identifies are obsolete; they are defined to maintain the backward compatibility.
 (define add-newp newp-add)
-(define clear-newp newp-add)
+(define clear-newp newp-clear)
 
+; (define a (newp-object process: (newp "sleep" "10" ) server-name: "hello" port-name: "bar" ))
 
+(define-class newp-object ()
+  (process)
+  (server-name)
+  (port-name)
+  ((stop) 
+   (kilp (this):process )))
